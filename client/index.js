3adf824c420d006df0e302bb6d04598a0dbca1c9
Andrew Morris <andrew.e.morris@gmail.com>
2021-12-16 18:33:04 +0000

    First commit

--- client/index.js
+++ client/index.js
@@ -2,6 +2,13 @@ import "./index.scss";
 
 const server = "http://localhost:3042";
 
+//PKI Initiation
+const EC = require('elliptic').ec;
+const SHA256 = require('crypto-js/sha256');
+const ec = new EC('secp256k1');
+console.log ("Runing verion 1");
+
+
 document.getElementById("exchange-address").addEventListener('input', ({ target: {value} }) => {
   if(value === "") {
     document.getElementById("balance").innerHTML = 0;
@@ -16,15 +23,29 @@ document.getElementById("exchange-address").addEventListener('input', ({ target:
 });
 
 document.getElementById("transfer-amount").addEventListener('click', () => {
+  console.log("Transfer pressed");
   const sender = document.getElementById("exchange-address").value;
   const amount = document.getElementById("send-amount").value;
   const recipient = document.getElementById("recipient").value;
+  //Read the privatekey
+  const privateKey = document.getElementById("privateKey").value;
+  const key = ec.keyFromPrivate(privateKey);
 
-  const body = JSON.stringify({
+  const oldBody = JSON.stringify({
     sender, amount, recipient
   });
+  console.log ("Body sans sig  =" + oldBody);
+
+  //Sign body message
+  const bodyHash = SHA256(oldBody);
+  const signature = key.sign(bodyHash.toString());
+  const body = JSON.stringify({
+    sender, amount, recipient, signature
+  });
+  console.log ("Body with sig  =" + body);
 
   const request = new Request(`${server}/send`, { method: 'POST', body });
+  console.log("Call transfer...")
 
   fetch(request, { headers: { 'Content-Type': 'application/json' }}).then(response => {
     return response.json();
--- server/index.js
+++ server/index.js
@@ -8,23 +8,95 @@ const port = 3042;
 app.use(cors());
 app.use(express.json());
 
+//Eliptic Library intiation
+const EC = require('elliptic').ec;
+const SHA256 = require('crypto-js/sha256');
+// Create and initialize EC context
+// (better do it once and reuse it)
+const ec = new EC('secp256k1');
+const key1 = ec.genKeyPair();
+const key2 = ec.genKeyPair();
+const key3 = ec.genKeyPair();
+
+const  publicKey1 = key1.getPublic().encode('hex');
+const  publicKey2 = key2.getPublic().encode('hex');
+const  publicKey3 = key3.getPublic().encode('hex');
+
+console.log({
+  privateKey1: key1.getPrivate().toString(16),
+  privateKey2: key2.getPrivate().toString(16),
+  privateKey3: key3.getPrivate().toString(16),
+  public1: publicKey1,
+  public2: publicKey2,
+  public3: publicKey3,
+  publicX1: key1.getPublic().x.toString(16),
+  publicX2: key2.getPublic().x.toString(16),
+  publicX3: key3.getPublic().x.toString(16),
+  publicY1: key1.getPublic().y.toString(16),
+  publicY2: key2.getPublic().y.toString(16),
+  publicY3: key3.getPublic().y.toString(16),
+});
+
+
+
 const balances = {
-  "1": 100,
-  "2": 50,
-  "3": 75,
+  [publicKey1]: 100,
+  [publicKey2]: 100,
+  [publicKey3]: 100,
 }
 
 app.get('/balance/:address', (req, res) => {
   const {address} = req.params;
   const balance = balances[address] || 0;
+  console.log("Balance " + balance)
   res.send({ balance });
 });
 
 app.post('/send', (req, res) => {
-  const {sender, recipient, amount} = req.body;
+  console.log("Send requested")
+  const {sender, recipient, amount, signature} = req.body;
+  console.log("req.body = " + req.body);
+  console.log("sender "+ sender);
+  console.log("recipient "+ recipient);
+  console.log("amount "+ amount);
+  console.log("signature "+ signature);
+  console.log({
+    sender: req.body.sender,
+    recipient: req.body.recipient,
+    amount: req.body.amount,
+    signature: req.body.signature,
+  });
+  //Remove the signature form the message, to recareas the signed body
+    // begin to construct independent message in the server
+    const message = JSON.stringify({
+      sender: sender,
+      amount: amount,
+      recipient: recipient,
+    });
+    // hash the independent message
+    console.log ("Receinf body to hash  =" + message);
+  const messageHash = SHA256(message).toString();
+  let publicKeyMatch = true;
+
+  // if recoverPublicKey() returns correct public key contained in db, else mark false
+  if(!balances[sender]) {
+    console.error("Public key does not match! Make sure you are passing in the correct values!");
+    publicKeyMatch = false;
+  }
+  
+  console.log(sender + " is attempting to send " + amount + " to " + recipient);
+  //const isSigned = secp.verify(signature, messageHash, recoveredPublicKey);
+  const key = ec.keyFromPublic(sender, 'hex');
+
+  //Very that the signed body = signature
+  const signed = (key.verify(messageHash, signature));
+  console.log ("signed " + signed); 
+  if (signed){ 
+
   balances[sender] -= amount;
   balances[recipient] = (balances[recipient] || 0) + +amount;
   res.send({ balance: balances[sender] });
+  }
 });
 
 app.listen(port, () => {
